f7f6748399049aadabeac88e9687bc1fbe2ef8ac
hproctor021 <hproctor021@gmail.com>
2020-09-02 11:14:38 -0500

    Zoo submission

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,41 @@
 class Zoo
-
-end
+    attr_reader :name, :location
+   
+    @@all = []
+   
+    def initialize(name, location)
+       @name = name
+       @location = location
+       self.class.all << self
+    end
+   
+    def self.all
+       @@all
+    end
+   
+   
+    def animals
+     Animal.all.select {|a| a.zoo == self}
+    end
+   
+   
+    def animal_species
+       Animal.all.collect {| a | a.species}.uniq
+    end
+   
+   
+    def find_by_species(species)
+       Animal.all.select {| a | a.species == species }
+    end
+   
+   
+      def animal_nicknames
+         Animal.all.select {| anis | anis.zoo == self }.select {|a| return a.nickname}
+      end
+   
+   
+    def self.find_by_location(location)
+       Zoo.all.select {| zoolo | zoolo.location == location }
+    end
+   
+   end
--- run.rb
+++ run.rb
@@ -2,8 +2,19 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+z1 = Zoo.new("Niabi Zoo", "Illinois")
+z2 = Zoo.new("Broynx Zoo", "New York")
+z3 = Zoo.new("Brookfield Zoo", "Illinois")
+z4 = Zoo.new("Zoological Garden", "Berlin")
 
-#Test your code here
+
+a1 = Animal.new("Tiger", 45, "Domino", z2)
+a2 = Animal.new("Howler Monkey", 8, "Hootie", z3)
+a3 = Animal.new("Walrus", 576, "Walnut", z4)
+a4 = Animal.new("Zebra", 195, "Maru", z1 )
+a5 = Animal.new("Tiger", 324, "Mindy", z2)
+a6 = Animal.new("Elephant", 1200, "Emrald", z3)
+
 
 
 binding.pry
